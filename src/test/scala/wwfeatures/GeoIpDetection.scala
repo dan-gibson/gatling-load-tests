e8a5fbb01c3b2cf4c9609aafad80cdba84f8f8cd
AidyLewis <adrian_lewis@fastmail.fm>
2014-09-19 21:10:00 +0000

    renamed package

---
+++ src/test/scala/wwfeatures/GeoIpDetection.scala
@@ -0,0 +1,25 @@
+package wwfeatures
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class GeoIpDetection extends Simulation {
+
+  val httpProtocol = http
+    .baseURL("http://www.stage.bbc.com")
+    .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
+    .acceptHeader("""text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""")
+    .acceptEncodingHeader("""gzip, deflate""")
+    .acceptLanguageHeader("""en-gb,en;q=0.5""")
+    .connection("""keep-alive""")
+    .userAgentHeader("""Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0""")
+
+    val scn = scenario("Earth")
+      .exec(http("earth")
+      .get("""/"""))
+
+    setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+}
