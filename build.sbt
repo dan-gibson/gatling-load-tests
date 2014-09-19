b41060c5a4b8142f132b2cd8b1198b55c1e12480
AidyLewis <adrian_lewis@fastmail.fm>
2014-09-19 20:18:33 +0000

    this sample test works fine on Linux

--- build.sbt
+++ build.sbt
@@ -1,2 +1,5 @@
-scalaVersion := "2.10.4"
+val test = project.in(file("."))
+  .enablePlugins(GatlingPlugin)
+    .settings(libraryDependencies += "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.0.0-RC5" % "test")
+    .settings(libraryDependencies += "io.gatling" % "test-framework" % "1.0-RC5" % "test")
 
--- project/Build.scala
+++
@@ -1,6 +0,0 @@
-import io.gatling.sbt.GatlingPlugin
-
-lazy val project = Project(...)
-                     .enablePlugins(GatlingPlugin)
-                     .settings(libraryDependencies ++= "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.0.0-RC5" % "test")
-                     .settings(libraryDependencies ++= "io.gatling" % "test-framework" % "0.0-RC5" % "test")
