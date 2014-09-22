04652c89818a9271d1944e72f2c06123991ed3cd
AidyLewis <adrian_lewis@fastmail.fm>
2014-09-22 18:28:34 +0100

    the logs show greater than 500 users, so trying just rampUsers

--- src/test/scala/mediaselector/MediaSelector.scala
+++ src/test/scala/mediaselector/MediaSelector.scala
@@ -21,6 +21,6 @@ class MediaSelector extends Simulation {
     ) 
 
       setUp(scn.inject(
-        rampUsersPerSec(1) to(500) during(10.minutes)
-        ).protocols(httpProtocol))
+        rampUsers(500) over(10.minutes)
+      ).protocols(httpProtocol))
 }
