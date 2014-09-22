9ec7b884a6c67944fdabc0882f81eb977ace3ead
AidyLewis <adrian_lewis@fastmail.fm>
2014-09-22 18:39:12 +0100

    commands to set up an instance

---
+++ support/ec2/set-up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is a set-up script for running Gatling on a BBC CentOS build.
+
+echo "installing wget"
+sudo yum install -y wget
+
+echo "download SBT 0.13.6 RPM"
+wget "https://dl.bintray.com/sbt/rpm/sbt-0.13.6.rpm"
+
+echo "Installing SBT RPM"
+sudo yum install -y "sbt-0.13.6.rpm"
