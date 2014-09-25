76982380a9a18674ffc8502d112de788a7cbb693
AidyLewis <adrian_lewis@fastmail.fm>
2014-09-25 12:57:05 +0100

    search suggest tests

---
+++ src/test/scala/search-suggest/SearchSuggest.scala
@@ -0,0 +1,28 @@
+package searchsuggest
+
+import scala.concurrent.duration._
+
+import scala.language.postfixOps
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class SearchSuggest extends Simulation {
+
+    val httpProtocol = http
+      .baseURL("http://open.stage.cwwtf.bbc.co.uk")
+      .connection("keep-alive")
+
+    val uniqueSuggestionsData = csv("unique_suggestions.csv").circular
+
+    val scn = scenario("Search Suggest")
+      .feed(uniqueSuggestionsData)
+      .exec(http("Search Suggest Query").get("/search-suggest/suggest?q=${uniqueSuggestions}&mediatype=video&scope=iplayer&format=blq-1")
+      .check(status.is(200)))
+
+      setUp(scn.inject(
+        rampUsersPerSec(10) to(200) during(3 minutes),
+        constantUsersPerSec(200) during(17 minutes)
+      ).protocols(httpProtocol))
+}
