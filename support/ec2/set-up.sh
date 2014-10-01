7d7e01899cfd4de3092794fb44c5a47fb0850f0d
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-01 15:10:21 +0100

    set-up tuning

--- support/ec2/set-up.sh
+++ support/ec2/set-up.sh
@@ -15,7 +15,7 @@ echo "Installing SBT RPM"
 sudo yum install -y "sbt-0.13.6.rpm"
 
 echo "Linux Tuning"
-sudo sh -c "printf '*       soft    nofile  65535 \n*       hard    nofile  65535' >> /etc/security/limits.conf"
+printf '*       soft    nofile  65535 \n*       hard    nofile  65535' | sudo tee /etc/security/limits.conf
 sudo sh -c "printf 'UseLogin yes' >> /etc/ssh/sshd_config" 
 # more ports for testing
 sudo sysctl -w net.ipv4.ip_local_port_range="1025 65535"
@@ -24,3 +24,5 @@ echo 300000 | sudo tee /proc/sys/fs/nr_open
 echo 300000 | sudo tee /proc/sys/fs/file-max
 
 exit
+
+# Might be an idea to now reboot
