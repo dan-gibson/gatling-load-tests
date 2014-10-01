3b9889b3f6f6e1a03e36c6638fd1f133104e411b
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-01 12:35:36 +0100

    added Linux tuning

--- support/ec2/set-up.sh
+++ support/ec2/set-up.sh
@@ -14,4 +14,13 @@ wget "https://dl.bintray.com/sbt/rpm/sbt-0.13.6.rpm"
 echo "Installing SBT RPM"
 sudo yum install -y "sbt-0.13.6.rpm"
 
+echo "Linux Tuning"
+sudo sh -c "printf '*       soft    nofile  65535 \n*       hard    nofile  65535' >> /etc/security/limits.conf"
+sudo sh -c "printf 'UseLogin yes' >> /etc/ssh/sshd_config" 
+# more ports for testing
+sudo sysctl -w net.ipv4.ip_local_port_range="1025 65535"
+# increase the maximum number of possible open file descriptors:
+echo 300000 | sudo tee /proc/sys/fs/nr_open
+echo 300000 | sudo tee /proc/sys/fs/file-max
+
 exit
