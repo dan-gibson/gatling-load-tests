495c43ec77f1ec565afe7297b4a64256e8a393bb
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-07 16:32:56 +0100

    basic gatling sample

---
+++ src/test/scala/bbc/sample/NewsSample.scala
@@ -0,0 +1,27 @@
+package bbc.sample
+
+import scala.concurrent.duration._
+
+import scala.util.matching.Regex
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class NewsSample extends Simulation {
+
+  val httpProtocol = http
+    .baseURL("http://www.stage.bbc.co.uk")
+    .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
+    .acceptHeader("""text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""")
+    .acceptEncodingHeader("""gzip, deflate""")
+    .acceptLanguageHeader("""en-gb,en;q=0.5""")
+    .connection("""keep-alive""")
+    .userAgentHeader("""Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0""")
+
+    val scn = scenario("News")
+      .exec(http("news-homepage")
+      .get("""/""")
+      .check(status.is(200)))
+    setUp(scn.inject(atOnceUsers(10)).protocols(httpProtocol))
+}
