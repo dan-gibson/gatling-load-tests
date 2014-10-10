53da7b9f60df1610abd3ff833a9727b7567598b3
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-10 13:10:01 +0100

    basic script to report on RPS

--- support/scripts/print_metrics.py
+++ support/scripts/print_metrics.py
@@ -1,8 +1,17 @@
 import sys
+import re
+
+def parse_data(data):
+  if data:
+    match = re.search(r"allRequests.all.count (\d+)", input)
+    if match:
+      sys.stdout.write("RPS: " + match.group(1) + "\n")
+
 while 1:
     try:
         input = sys.stdin.readline()
-        if input:
-            sys.stdout.write(input)
+        parse_data(input)
     except KeyboardInterrupt:
          sys.exit()
+
+
