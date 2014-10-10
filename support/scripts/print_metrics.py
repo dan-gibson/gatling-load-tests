83f95d56b1c6cabdd4509e76ee12aaa982aaa50f
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-10 15:13:26 +0100

    basic print data script

--- support/scripts/print_metrics.py
+++ support/scripts/print_metrics.py
@@ -1,11 +1,19 @@
+#!/usr/bin/python
+
 import sys
 import re
 
 def parse_data(data):
   if data:
-    match = re.search(r"allRequests.all.count (\d+)", input)
-    if match:
-      sys.stdout.write("RPS: " + match.group(1) + "\n")
+    count = re.search(r"allRequests.all.count (\d+)", input)
+    minimum = re.search(r"allRequests.all.min (\d+)", input)
+    maximum = re.search(r"allRequests.all.max (\d+)", input)
+    if count: 
+      sys.stdout.write("RPS: " + count.group(1) + "\n")
+    if minimum:
+      sys.stdout.write("Min: " + minimum.group(1) + "\n")
+    if maximum:
+      sys.stdout.write("Max: " + maximum.group(1) + "\n\n")
 
 while 1:
     try:
