478cec2ef516ba4798a28f7b8771248de20e9f7f
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-13 11:24:20 +0100

    printing min, max, rps

--- support/scripts/print_metrics.py
+++ support/scripts/print_metrics.py
@@ -4,25 +4,25 @@ import sys
 import re
 
 def parse_data(data):
-    count, minimum, maximum  = re.search(r"allRequests.all.count (\d+)", data), \
+    count, minimum, maximum = re.search(r"allRequests.all.count (\d+)", data), \
         re.search(r"allRequests.all.min (\d+)", data), re.search(r"allRequests.all.max (\d+)", data)
+    return count, minimum, maximum
 
-def print_data()
-  parse_data
-    
-    # if count: 
-    #   sys.stdout.write("RPS: " + count.group(1) + "\n")
-    # if minimum:
-    #   sys.stdout.write("Min: " + minimum.group(1) + "\n")
-    # if maximum:
-    #   sys.stdout.write("Max: " + maximum.group(1) + "\n\n")
+def print_data(data):
+    count, minimum, maximum = parse_data(data)
+    if count: 
+      sys.stdout.write("RPS: " + count.group(1) + "\n")
+    if minimum:
+      sys.stdout.write("Min: " + minimum.group(1) + "\n")
+    if maximum:
+      sys.stdout.write("Max: " + maximum.group(1) + "\n\n")
 
-# while 1:
-    # try:
-    #     input = sys.stdin.readline()
-    #     if input:
-    #     parse_data(input)
-    # except KeyboardInterrupt:
-    #      sys.exit()
+while True:
+    try:
+        input = sys.stdin.readline()
+        if input:
+            print_data(input)
+    except KeyboardInterrupt:
+         sys.exit()
 
 
