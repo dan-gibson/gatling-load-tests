531d9c1d96544049c88ea315bf4657410a4bb004
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-17 11:18:18 +0100

    print formatting

--- support/scripts/print_metrics.py
+++ support/scripts/print_metrics.py
@@ -29,8 +29,9 @@ def parse_data():
 def print_data():
     count, percentile_95, epoch = parse_data()
     datetime = convert_epoch_to_datetime(epoch.group(1))
-    sys.stdout.write("datetime, RPS, 95% \n")
-    sys.stdout.write(datetime + ", " + count.group(1) + ", " + percentile_95.group(1) + "\n\n")
+    header = "datetime".ljust(19) + "  RPS  95% \n" 
+    sys.stdout.write(header)
+    sys.stdout.write(datetime + ", " + count.group(1) + ",  " + percentile_95.group(1) + "\n\n")
 
 while True:
     try:
