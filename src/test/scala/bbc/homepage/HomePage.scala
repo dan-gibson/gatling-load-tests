699811678837345dd9dc44b8edf168a929579bbb
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-21 17:53:44 +0100

    moved to resources. Much cleaner

--- src/test/scala/bbc/homepage/HomePage.scala
+++ src/test/scala/bbc/homepage/HomePage.scala
@@ -22,24 +22,22 @@ class HomePage extends Simulation {
     val locationId = csv("homepage/location-id.csv").circular
     val newsRegionId = csv("homepage/news-region-id.csv").circular
 
-    val scn = scenario("HomePage")
+    val scn = scenario("Homepage")
         .feed(locCookie)
         .feed(locationId)
         .feed(newsRegionId)
         .exec(addCookie(Cookie("locserv", "${locCookie}")))
-        .group("Domestic") { 
-            exec(http("Domestic").get("""/""").check(status.is(200)))
-            .exec(http("Location Id")
-            .get("""/home/four/modules/h4weather/domestic/${locationId}/Trafford%2BPark/en-GB""").check(status.is(200))
-            .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0"))
-            .exec(http("News Region Id")
-            .get("""/home/four/modules/h4discoveryzone/1/domestic/default/default/${newsRegionId}/en-GB.json""")
-            .check(status.is(200))
-            .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0"))
-        }
 
-      setUp(scn.inject(
-          constantUsersPerSec(10) during(10 seconds)
+        .exec(http("Domestic").get("""/""")
+        .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0")
+        .resources(
+          http("weather").get("""/home/four/modules/h4weather/domestic/${locationId}/Trafford%2BPark/en-GB"""),
+          http("json").get("""/home/four/modules/h4discoveryzone/1/domestic/default/default/${newsRegionId}/en-GB.json"""))) 
+
+
+
+      setUp(
+        scn.inject(constantUsersPerSec(10) during(10 seconds)
       ).protocols(httpProtocol))
 }
 
