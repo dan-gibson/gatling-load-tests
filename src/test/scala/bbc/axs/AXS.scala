a9c9c6028c184b59fe401e3c3c4fe7f6f9ef4605
AidyLewis <adrian_lewis@fastmail.fm>
2014-10-22 11:43:23 +0100

    AXS test and data files

--- src/test/scala/bbc/axs/AXS.scala
+++ src/test/scala/bbc/axs/AXS.scala
@@ -8,22 +8,22 @@ import io.gatling.jdbc.Predef._
 
 class AXS extends Simulation {
 
-  val httpProtocol = http
-    .baseURL("http://open.stage.bbc.co.uk/axs/open/authxml")
+    val httpProtocol = http
+        .baseURL("http://open.stage.bbc.co.uk/axs/open/authxml")
 
     val mediaSet = csv("axs/media-set.csv").circular
-    val versionPid = csv("axs/version-pid").circular
+    val versionPid = csv("axs/version-pid.csv").circular
 
     val scn = scenario("AXS")
-      .feed(mediaSet)
-      .feed(versionPid)
-      .exec(http("AXS")
-      .get("?media_set=${mediaSet}&version_pid=${versionPid}&format=base64")
-      .check(status.is(200))
+        .feed(mediaSet)
+        .feed(versionPid)
+        .exec(http("AXS")
+        .get("?media_set=${mediaSet}&version_pid=${versionPid}&format=base64")
+        .check(status.is(200))
     ) 
 
       setUp(scn.inject(
-        rampUsersPerSec(10) to(100) during(3 minutes) randomized,
-        constantUsersPerSec(100) during(7 minutes) randomized
+          rampUsersPerSec(10) to(100) during(3 minutes) randomized,
+          constantUsersPerSec(100) during(7 minutes) randomized
       ).protocols(httpProtocol))
 }
